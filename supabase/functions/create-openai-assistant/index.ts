96e1f460ddbf091ac4d4bce5103d73b2f798cf0d
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-02-21 01:19:21 -0500

    Create the create-openai-assistant function

---
+++ supabase/functions/create-openai-assistant/index.ts
@@ -0,0 +1,42 @@
+import OpenAI from "openai";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+};
+
+async function CreateOpenaiAssistantHandler(req: Request) {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    const { file_id } = await req.json();
+
+    const openai = new OpenAI({
+      apiKey: Deno.env.get("OPENAI_API_KEY"),
+    });
+
+    const assistant = await openai.beta.assistants.create({
+      name: "Resume Assistant",
+      description:
+        "As a Resume Assistant, your mission is to assist users in refining their resumes for job applications. You're tasked with analyzing resumes to suggest improvements, tailoring content to align with job descriptions, offering guidance on effective writing and formatting, and answering queries related to resume optimization. Maintain a professional yet supportive tone throughout interactions, focusing on providing actionable advice. It's crucial to respect user privacy by treating all personal information with confidentiality, aiming to bolster user confidence in their job search journey.",
+      model: "gpt-4-turbo-preview",
+      tools: [{ "type": "code_interpreter" }],
+      file_ids: [file_id],
+    });
+
+    return new Response(JSON.stringify(assistant), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    // Return error message
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+}
+
+Deno.serve(CreateOpenaiAssistantHandler);
