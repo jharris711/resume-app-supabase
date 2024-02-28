616241c12fcfafda0f0c86507230e142ff3e5389
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-02-27 21:22:26 -0500

    remove console logs

--- supabase/functions/resumes-api/delete-resume.ts
+++ supabase/functions/resumes-api/delete-resume.ts
@@ -11,7 +11,6 @@ const RESUMES_BUCKET = "resumes";
 const RESUMES_TABLE = "resumes";
 
 export async function deleteResume({ supabaseClient, id }: Props) {
-  console.log("id", id);
   const { data: getResumeResponse, error: getResumeError } =
     await supabaseClient
       .from(
@@ -22,31 +21,21 @@ export async function deleteResume({ supabaseClient, id }: Props) {
       )
       .eq("id", id);
 
-  console.log("getResumeResponse", getResumeResponse);
-
   if (getResumeError) throw getResumeError;
 
   const resume = getResumeResponse[0];
 
-  console.log("resume", resume);
-
-  const { data: deleteResumeResponse, error: deleteResumeError } =
-    await supabaseClient
-      .from(RESUMES_TABLE)
-      .delete()
-      .eq("id", id);
-
-  console.log("deleteResumeResponse", deleteResumeResponse);
+  const { error: deleteResumeError } = await supabaseClient
+    .from(RESUMES_TABLE)
+    .delete()
+    .eq("id", id);
 
   if (deleteResumeError) throw deleteResumeError;
 
-  const { data: removeResumeResponse, error: removeResumeError } =
-    await supabaseClient
-      .storage
-      .from(RESUMES_BUCKET)
-      .remove([`resumes/${resume.file_name}`]);
-
-  console.log("removeResumeResponse", removeResumeResponse);
+  const { error: removeResumeError } = await supabaseClient
+    .storage
+    .from(RESUMES_BUCKET)
+    .remove([`resumes/${resume.file_name}`]);
 
   if (removeResumeError) throw removeResumeError;
 
